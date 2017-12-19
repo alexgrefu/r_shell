4f445ebd83db82d558300f5f4942dbcafba0db0e
Alex Grefu <alex.grefu@jlg.ro>
2017-12-19 16:46:10 +0200

    sending commands

--- server.py
+++ server.py
@@ -16,31 +16,42 @@ def create_socket():
         print("Error creating socket: " + str(msg))
 
 
-
-
 def bind_socket(call_count):
-
-
-
     try:
-
-        global host
-        global port
-        global s
-
         print("Binding the socket to " + str(port))
 
         s.bind((host, port))
         s.listen(5)
+
     except socket.error as msg:
         print("Error in binding the socket: " + str(msg) + "\nRetrying... ")
-        if (call_count > 0):
+        if call_count > 0:
             bind_socket(call_count - 1)
 
+
+def accept_socket():
+    print('Accepting')
+    conn, address = s.accept()
+    print("Connection has been established... IP: " + address[0] + " PORT: " + str(address[1]))
+    send_commands(conn)
+    conn.close()
+
+def send_commands(conn):
+    while True:
+        cmd = input()
+        if cmd == 'quit':
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024), 'utf-8')
+            print(client_response, end='')
 
 if __name__ == '__main__':
     create_socket()
     bind_socket(call_count=3)
+    accept_socket()
 
 
 
