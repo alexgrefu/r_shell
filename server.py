d10a414e0cd76198c205506253d627af4b987ce8
Alex Grefu <alex.grefu@jlg.ro>
2017-12-19 18:24:48 +0200

    client side implementation

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ def create_socket():
         global s
 
         host = ""
-        port = 31377
+        port = 9999
         s = socket.socket()
     except socket.error as msg:
         print("Error creating socket: " + str(msg))
@@ -30,11 +30,14 @@ def bind_socket(call_count):
 
 
 def accept_socket():
-    print('Accepting')
-    conn, address = s.accept()
-    print("Connection has been established... IP: " + address[0] + " PORT: " + str(address[1]))
-    send_commands(conn)
-    conn.close()
+    try:
+        conn, address = s.accept()
+        print("Connection has been established... IP: " + address[0] + " PORT: " + str(address[1]))
+        send_commands(conn)
+        conn.close()
+    except socket.error as msg:
+        print(msg)
+
 
 def send_commands(conn):
     while True:
@@ -47,6 +50,7 @@ def send_commands(conn):
             conn.send(str.encode(cmd))
             client_response = str(conn.recv(1024), 'utf-8')
             print(client_response, end='')
+
 
 if __name__ == '__main__':
     create_socket()
